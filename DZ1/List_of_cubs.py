34636cd13a139cad2f9093f3fb4858e364876d6a
Lexan <sheklford2015@yandex.ru>
2021-05-02 14:31:40 +0300

    request of list of cubs

---
+++ DZ1/List_of_cubs.py
@@ -0,0 +1,19 @@
+numbers = range(1, 1000)
+list_of_cubs = []
+
+for number in numbers:
+    # Берем нечетные числа
+    if number % 2 != 0:
+        # Возводим в куб (пункт a задания), прибавляем 17 (пункт b). Новый список не создан (пункт c)
+        number = number**3 + 17
+        # складываем нехитрыми манипуляциями в цикле while, где num само число для цикла, amount - сумма элементов
+        num = number
+        amount = 0
+        while num != 0:
+            amount = amount + num % 10
+            num = num // 10
+        # проверяем делится ли на 7 без остатка, если делится заносим в список
+        if amount % 7 == 0:
+            list_of_cubs.append(number)
+
+print(list_of_cubs)
